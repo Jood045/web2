37659cf51d4a530765598c2089ff7a9e0f7a4518
Jood <445201348@student.ksu.edu.sa>
2026-03-29 09:59:19 +0300

    Add files via upload

--- kidbites.sql
+++ kidbites.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 28 مارس 2026 الساعة 19:22
+-- Generation Time: 29 مارس 2026 الساعة 06:58
 -- إصدار الخادم: 5.7.24
 -- PHP Version: 8.3.1
 
@@ -63,18 +63,9 @@ CREATE TABLE `comment` (
 --
 
 CREATE TABLE `favourites` (
-  `userID` int(11) DEFAULT NULL,
-  `recipeID` int(11) DEFAULT NULL
+  `userID` int(11) NOT NULL,
+  `recipeID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- إرجاع أو استيراد بيانات الجدول `favourites`
---
-
-INSERT INTO `favourites` (`userID`, `recipeID`) VALUES
-(1, 1),
-(1, 5),
-(2, 2);
 
 -- --------------------------------------------------------
 
@@ -86,7 +77,7 @@ CREATE TABLE `ingredients` (
   `id` int(11) NOT NULL,
   `recipeID` int(11) DEFAULT NULL,
   `ingredientName` varchar(100) DEFAULT NULL,
-  `ingredientQuantity` varchar(100) DEFAULT NULL
+  `ingredientQuantity` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -109,29 +100,9 @@ CREATE TABLE `instructions` (
 --
 
 CREATE TABLE `likes` (
-  `userID` int(11) DEFAULT NULL,
-  `recipeID` int(11) DEFAULT NULL
+  `userID` int(11) NOT NULL,
+  `recipeID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- إرجاع أو استيراد بيانات الجدول `likes`
---
-
-INSERT INTO `likes` (`userID`, `recipeID`) VALUES
-(1, 1),
-(2, 1),
-(3, 1),
-(4, 1),
-(1, 2),
-(2, 2),
-(3, 2),
-(1, 3),
-(2, 3),
-(1, 4),
-(2, 4),
-(1, 5),
-(2, 5),
-(3, 5);
 
 -- --------------------------------------------------------
 
@@ -154,11 +125,10 @@ CREATE TABLE `recipe` (
 --
 
 INSERT INTO `recipe` (`id`, `userID`, `categoryID`, `name`, `description`, `photoFileName`, `videoFilePath`) VALUES
-(1, 6, 2, 'Rainbow Veggie Pasta Cups', 'Warm, cozy pasta cups that parents trust and kids enjoy. Great for school lunchboxes and quick dinners.\r\n', 'pasta.jpg', 'https://youtu.be/8UQkmNW5jec?si=PbSU4QBJX5a9yWRU'),
-(2, 6, 1, 'Banana Oat Pancakes', 'Soft and healthy pancakes made with simple ingredients, perfect for picky kids.', 'banana-pancakes.jpg', 'https://youtu.be/Stpe90s2pD4?si=Ky3jBuOxqy9zc5yd'),
-(3, 7, 2, 'Chicken & Avocado Mini Wraps', 'Small wraps filled with chicken and avocado, easy to eat and nutritious.', 'chicken-wraps.jpg', 'No Video'),
-(4, 6, 3, 'Apple Peanut Butter Sandwich Bites', 'Apple slices with peanut butter, shaped like mini sandwiches.', 'apple-sandwich.jpg', 'https://youtu.be/ANKtVORw05U?si=DDzevPe9DAggxJqQ'),
-(5, 8, 1, 'Egg & Cheese Breakfast Muffins', 'Mini egg muffins that are easy to prepare and great for busy mornings.', 'egg-muffins.jpg', 'No Video');
+(1, 6, 2, 'Rainbow Veggie Pasta Cups', 'Warm, cozy pasta cups that parents trust and kids love', 'pasta.jpg', ''),
+(2, 11, 1, 'Banana Oat Pancakes', 'Soft and healthy pancakes made with simple ingredients', 'banana-pancakes.jpg', ''),
+(3, 6, 3, 'Apple Peanut Butter Sandwich Bites', 'Apple slices with peanut butter shaped like mini burgers', 'apple-sandwich.jpg', ''),
+(4, 6, 1, 'Egg & Cheese Breakfast Muffins', 'Mini egg muffins that are easy to prepare', 'egg-muffins.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -168,7 +138,7 @@ INSERT INTO `recipe` (`id`, `userID`, `categoryID`, `name`, `description`, `phot
 
 CREATE TABLE `recipecategory` (
   `id` int(11) NOT NULL,
-  `categoryName` varchar(100) DEFAULT NULL
+  `categoryName` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -213,10 +183,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `userType`, `firstName`, `lastName`, `emailAddress`, `password`, `photoFileName`) VALUES
-(6, 'user', 'shatha', 'mana', 'Shatha@gmail.com', '$2y$10$n3kz8.s0E9dw0/1vpCDuDeBkIN8SkVJB0jgcI4ujFkQd62ORHHbQe', 'default.png'),
+(6, 'user', 'shatha', 'mana', 'Shatha@gmail.com', '$2y$10$n3kz8.s0E9dw0/1vpCDuDeBkIN8SkVJB0jgcI4ujFkQd62ORHHbQe', 'sara.jpg'),
 (7, 'user', 'Jood', 'Abdullah', 'jood@gmail.com', '$2y$10$gABwfjcLFzfy2alxkaYMqOahV.K8MeUU69Q.nVzrVu9ReA.ZzFV52', 'Sara.jpg'),
 (8, 'user', 'Jood', 'Abdullah', 'jood5@gmail.com', '$2y$10$xXJRHluaYAsNgUayuhFJ0enloX33.Tsk94v6QlwaRNbzmHGzaQzhS', 'default.png'),
-(9, 'user', 'ghada', 'f', 'RRR@GMAIL.COM', '$2y$10$68errXAPtJVuXdm5GHvWqOAhvRfqVxkHkFVh0pnmY7W4l1t3eq0oK', 'Ghada.jpg');
+(9, 'user', 'ghada', 'f', 'RRR@GMAIL.COM', '$2y$10$68errXAPtJVuXdm5GHvWqOAhvRfqVxkHkFVh0pnmY7W4l1t3eq0oK', 'Ghada.jpg'),
+(10, 'user', 'lama', 'L', 'L@gmail.com', '$2y$10$K71hu.4FIuVvHJeeJtlTb.6Lt77kc9BJLAUXdav2t926BzxPFxq3W', 'default.png'),
+(11, 'user', 'sara', 's', 's@gmail.com', '$2y$10$TRqhT.W.uIvEZS3afBMQueb.wgw1bukUe7REydole6R1zQSGGpLAa', 'user.jpg');
 
 --
 -- Indexes for dumped tables
@@ -233,25 +205,45 @@ ALTER TABLE `blocked_users`
 -- Indexes for table `comment`
 --
 ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `recipeID` (`recipeID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `favourites`
+--
+ALTER TABLE `favourites`
+  ADD PRIMARY KEY (`userID`,`recipeID`),
+  ADD KEY `recipeID` (`recipeID`);
 
 --
 -- Indexes for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `recipeID` (`recipeID`);
 
 --
 -- Indexes for table `instructions`
 --
 ALTER TABLE `instructions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `recipeID` (`recipeID`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`userID`,`recipeID`),
+  ADD KEY `recipeID` (`recipeID`);
 
 --
 -- Indexes for table `recipe`
 --
 ALTER TABLE `recipe`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `categoryID` (`categoryID`);
 
 --
 -- Indexes for table `recipecategory`
@@ -263,7 +255,9 @@ ALTER TABLE `recipecategory`
 -- Indexes for table `report`
 --
 ALTER TABLE `report`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `recipeID` (`recipeID`);
 
 --
 -- Indexes for table `users`
@@ -304,7 +298,7 @@ ALTER TABLE `instructions`
 -- AUTO_INCREMENT for table `recipe`
 --
 ALTER TABLE `recipe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `recipecategory`
@@ -322,7 +316,58 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- قيود الجداول المحفوظة
+--
+
+--
+-- القيود للجدول `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`id`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`id`);
+
+--
+-- القيود للجدول `favourites`
+--
+ALTER TABLE `favourites`
+  ADD CONSTRAINT `favourites_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `favourites_ibfk_2` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`id`);
+
+--
+-- القيود للجدول `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD CONSTRAINT `ingredients_ibfk_1` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`id`);
+
+--
+-- القيود للجدول `instructions`
+--
+ALTER TABLE `instructions`
+  ADD CONSTRAINT `instructions_ibfk_1` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`id`);
+
+--
+-- القيود للجدول `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`id`);
+
+--
+-- القيود للجدول `recipe`
+--
+ALTER TABLE `recipe`
+  ADD CONSTRAINT `recipe_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `recipe_ibfk_2` FOREIGN KEY (`categoryID`) REFERENCES `recipecategory` (`id`);
+
+--
+-- القيود للجدول `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
